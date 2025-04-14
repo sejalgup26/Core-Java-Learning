3248f7ede1ff1b05ffd8c04fdffc939fe21eb4a9
unknown <guptasejal0521@gmail.com>
2025-04-14 20:06:34 +0530

    adding Sub.java

---
+++ Sub.java
@@ -0,0 +1,8 @@
+public class Sub {
+    public static void main(String[] args) {
+        int a1=20;
+        int a2=30;
+        int sub=a1-a2;
+        System.out.println("The Sub is"+sub);
+    }
+}
