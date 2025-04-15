74e9df68d36f18167dc80fd24d92b9ed5749bcbc
sejalgup26 <guptasejal0521@gmail.com>
2025-04-16 00:27:59 +0530

    New commit for triangle classification

---
+++ src/Lab002.java
@@ -0,0 +1,24 @@
+package src;
+
+import java.util.Scanner;
+//traingle clasifier
+public class Lab002 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 3 sides of triangle");
+        double s1= sc.nextDouble();  //side 1
+        double s2= sc.nextDouble();  //side 2
+        double s3=sc.nextDouble();   //side 3
+         if(s1==s2 && s2==s3 && s3==s1){
+             System.out.println("Equilateral triangle");
+         }
+         else if(s1==s2 || s2==s3 || s3==s1 ){
+             System.out.println("Isosceles triangle");
+         }
+         else {
+             System.out.println("Scalene triangle");
+         }
+
+
+    }
+}
