83760026d1de320b80365ddf320bc9f7de7655dd
unknown <guptasejal0521@gmail.com>
2025-04-15 23:12:02 +0530

    New commit

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a1=23;
+        int a2=12;
+        int mul=a1*a2;
+        System.out.println("Answer is "+mul);
+    }
+}
