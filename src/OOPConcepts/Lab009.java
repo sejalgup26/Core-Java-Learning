22b4418e3d9b444d99aba932dec17331002fa6c5
sejalgup26 <guptasejal0521@gmail.com>
2025-04-19 00:18:40 +0530

    19 April

---
+++ src/OOPConcepts/Lab009.java
@@ -0,0 +1,11 @@
+package src.OOPConcepts;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        int c=sum(3,6);
+        System.out.println(c);
+    }
+    static int sum(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/OOPConcepts/Lab010.java
@@ -0,0 +1,7 @@
+package src.OOPConcepts;
+
+public class Lab010 {
+    public static void main(String[] args) {
+
+    }
+}
