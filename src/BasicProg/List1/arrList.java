4a67177aad8c977b4569324cc7f3dd8c487f75f7
sejalgup26 <guptasejal0521@gmail.com>
2025-05-22 19:02:35 +0530

    New commit 2

--- src/BasicProg/List1/arrList.java
+++ src/BasicProg/List1/arrList.java
@@ -11,7 +11,13 @@ public class arrList {
         myList.add(90);
         System.out.println(myList);
         System.out.println(myList.get(1));
+myList.set(2,"Pari");
+        System.out.println(myList);
+        myList.remove("Sejal");
+        System.out.println(myList);
 
-
+        for(Object o:myList){
+            System.out.println(o);
+        }
     }
 }
