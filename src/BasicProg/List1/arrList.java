ef1b2f9a0e9fb8570ae92eea270aacbd0971b60d
sejalgup26 <guptasejal0521@gmail.com>
2025-05-22 18:19:45 +0530

    new comm

---
+++ src/BasicProg/List1/arrList.java
@@ -0,0 +1,17 @@
+package src.BasicProg.List1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class arrList {
+    public static void main(String[] args) {
+        List myList= new ArrayList();
+        myList.add("Pramod");
+        myList.add("Sejal");
+        myList.add(90);
+        System.out.println(myList);
+        System.out.println(myList.get(1));
+
+
+    }
+}
