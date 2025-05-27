d6e630264f4830d9abc5da5c00fba6506820fd60
sejalgup26 <guptasejal0521@gmail.com>
2025-05-28 01:03:38 +0530

    new coom

--- src/BasicProg/List1/Addarry.java
+++ src/BasicProg/List1/Addarry.java
@@ -2,17 +2,16 @@ package src.BasicProg.List1;
 
 import java.util.ArrayList;
 import java.util.List;
-
-///Write a Java program to create an array list, add some colors (strings) and print out the collection.
-public class Arr2 {
+//Insert at any index
+public class Addarry {
     public static void main(String[] args) {
         List<String> mylist=new ArrayList<>();
         mylist.add("Red");
         mylist.add("Yellow");
         mylist.add("Blue");
         mylist.add("Green");
-        //Write a Java program to iterate through all elements in an array list.
-        for(String element:mylist){
-        System.out.println(element);
-    }}
+
+        mylist.add(0,"Black");
+        System.out.println(mylist);
+    }
 }
