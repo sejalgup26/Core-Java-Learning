b47cab592e3aa3398ba2ed6f7d24dd0f7ab7824f
sejalgup26 <guptasejal0521@gmail.com>
2025-06-12 01:58:34 +0530

    q4 hash commit

---
+++ src/BasicProg/HashMap/q4.java
@@ -0,0 +1,15 @@
+package src.BasicProg.HashMap;
+
+import java.util.HashMap;
+
+public class q4 {
+    public static void main(String[] args) {
+        HashMap<Integer,String> pr=new HashMap<>();
+        pr.put(1,"SEjal");
+        pr.put(2,"Pari");
+        pr.put(3,"Gautmi");
+        pr.put(4,"sanvi");
+        pr.clear();
+        System.out.println(pr);
+    }
+}
