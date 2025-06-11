76df2c41cadf6c73203c7a97614d98731e19d740
sejalgup26 <guptasejal0521@gmail.com>
2025-06-12 01:42:25 +0530

    q2 hash commit

---
+++ src/BasicProg/HashMap/q2.java
@@ -0,0 +1,14 @@
+package src.BasicProg.HashMap;
+
+import java.util.HashMap;
+
+public class q2 {
+    public static void main(String[] args) {
+        HashMap<Integer,String> pr=new HashMap<>();
+        pr.put(1,"SEjal");
+        pr.put(2,"Pari");
+        pr.put(3,"Gautmi");
+        pr.put(4,"sanvi");
+        System.out.println("size of an hastable is "+pr.size());
+    }
+}
